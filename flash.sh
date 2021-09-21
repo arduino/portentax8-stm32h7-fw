d62c3c9986ea230f23bf8f3b447c0e5c773d8242
Martino Facchin <m.facchin@arduino.cc>
2021-09-21 12:33:56 +0200

    Amend flash.sh with forced gpio set as output
    Looks like newer images can't handle it correctly

--- flash.sh
+++ flash.sh
@@ -1,3 +1,10 @@
 # copy me to the linux board ;)
+
+# Bug in stm32flash binary, RESET is not being set correctly
+# CHANGEME: on actual hardware, the GPIO will likely be different
+echo 7 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio7/direction
+
+
 /home/fio/stm32flash -o /dev/spidev1.1 -i '8,-7,,7,,:-8,-7,,7'
 /home/fio/stm32flash -w /home/fio/STM32H747AII6_CM7.bin /dev/spidev1.1 -i '8,-7,,7,,:-8,-7,,7' -e 0
