f8c8d66cf6e20ea759e3d42258945dd41bef1543
Martino Facchin <m.facchin@arduino.cc>
2021-10-01 17:33:18 +0200

    Update flash script to x8

--- flash.sh
+++ flash.sh
@@ -2,9 +2,12 @@
 
 # Bug in stm32flash binary, RESET is not being set correctly
 # CHANGEME: on actual hardware, the GPIO will likely be different
-echo 7 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio7/direction
+echo 10 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio10/direction
+echo 1 > /sys/class/gpio/gpio10/value
+echo 11 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio11/direction
 
 
-stm32flash -o /dev/spidev1.0 -i '8,-7,,7,,:-8,-7,,7'
-stm32flash -w /home/fio/STM32H747AII6_CM7.bin /dev/spidev1.0 -i '8,-7,,7,,:-8,-7,,7' -e 0
+stm32flash -o /dev/spidev2.0 -i '11,-10,,10,,:-11,-10,,10'
+stm32flash -w /home/fio/STM32H747AII6_CM7.bin /dev/spidev2.0 -i '11,-10,,10,,:-11,-10,,10' -e 0
