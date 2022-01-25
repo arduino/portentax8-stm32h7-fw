b34b8c1f590783fde283c6a695a946649fb5752a
Massimo Pennazio <maxipenna@libero.it>
2022-01-25 17:48:36 +0100

    Use new cfg script with direct access to imx gpios

--- program.sh
+++ program.sh
@@ -3,12 +3,12 @@
 # copy me to the linux board ;)
 
 echo "Checking ext. rt mcu stm32h7 firmware version..."
-openocd -f /usr/arduino/extra/openocd_script.cfg -c "flash verify_image /usr/arduino/extra/STM32H747AII6_CM7.bin 0x8000000" -c "reset" -c "exit"
+openocd -f /usr/arduino/extra/openocd_script-imx_gpio.cfg -c "flash verify_image /usr/arduino/extra/STM32H747AII6_CM7.bin 0x8000000" -c "reset" -c "exit"
 res=$?
 
 if [ $res != 0 ]; then
     echo "Ext. rt mcu stm32h7 is not programmed/updated, starting programming procedure..."
-    openocd -f /usr/arduino/extra/openocd_script.cfg -c "program /usr/arduino/extra/STM32H747AII6_CM7.bin verify reset exit 0x8000000"
+    openocd -f /usr/arduino/extra/openocd_script-imx_gpio.cfg -c "program /usr/arduino/extra/STM32H747AII6_CM7.bin verify reset exit 0x8000000"
 else
     echo "Ext. rt mcu stm32h7 is programmed/updated, exiting..."
 fi
--- verify.sh
+++ verify.sh
@@ -2,4 +2,4 @@
 
 # copy me to the linux board ;)
 
-openocd -f /usr/arduino/extra/openocd_script.cfg -c "flash verify_image /usr/arduino/extra/STM32H747AII6_CM7.bin 0x8000000" -c "reset" -c "exit"
+openocd -f /usr/arduino/extra/openocd_script-imx_gpio.cfg -c "flash verify_image /usr/arduino/extra/STM32H747AII6_CM7.bin 0x8000000" -c "reset" -c "exit"
