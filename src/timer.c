37a47dcccebd7ac85ac8fe208cb6118fed6c9bf9
Alexander Entinger <cto@lxrobotics.com>
2022-03-18 11:28:30 +0100

    Fix: Removing unused variables.

--- src/timer.c
+++ src/timer.c
@@ -37,10 +37,6 @@ HRTIM_HandleTypeDef hhrtim;
 
 static void MX_HRTIM_Init(void) {
 
-  HRTIM_TimeBaseCfgTypeDef pTimeBaseCfg = {0};
-  HRTIM_TimerCfgTypeDef pTimerCfg = {0};
-  HRTIM_OutputCfgTypeDef pOutputCfg = {0};
-
   hhrtim.Instance = HRTIM1;
   hhrtim.Init.HRTIMInterruptResquests = HRTIM_IT_NONE;
   hhrtim.Init.SyncOptions = HRTIM_SYNCOPTION_NONE;
