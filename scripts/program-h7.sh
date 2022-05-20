ca59eca82d7fa70c30dc75fc5ed54135bae2fcc6
Massimo Pennazio <maxipenna@libero.it>
2022-05-20 11:03:25 +0200

    Fixing script indent

--- scripts/program-h7.sh
+++ scripts/program-h7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo 8 > /sys/class/gpio/export                                                 
-echo 15 > /sys/class/gpio/export                                                
-echo 10 > /sys/class/gpio/export                                                
+echo 8 > /sys/class/gpio/export
+echo 15 > /sys/class/gpio/export
+echo 10 > /sys/class/gpio/export
 
 FIRMWARE_H7_ON_MCU=$(cat /sys/kernel/x8h7_firmware/version)
 sudo -u fio dd if=/usr/arduino/extra/STM32H747AII6_CM7.bin of=/tmp/version bs=1 count=40 skip=$((0x40000))
@@ -17,4 +17,3 @@ else
   echo "Version(M8)="$FIRMWARE_H7_ON_LINUX
   openocd -f /usr/arduino/extra/openocd_script-imx_gpio.cfg -c "program /usr/arduino/extra/STM32H747AII6_CM7.bin verify reset exit 0x8000000"
 fi
-
