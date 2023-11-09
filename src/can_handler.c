de7f782b4d19f637ed4975bda7424e356c352719
Alexander Entinger <consulting@lxrobotics.com>
2023-11-09 09:25:06 +0100

    Define "x8h7_can_init_message" which contains the required information for configuring this nodes CAN module.

--- src/can_handler.c
+++ src/can_handler.c
@@ -35,6 +35,15 @@
  * TYPEDEF
  **************************************************************************************/
 
+union x8h7_can_init_message
+{
+  struct __attribute__((packed))
+  {
+    uint32_t can_bitrate_Hz;
+  } field;
+  uint8_t buf[sizeof(uint32_t) /* can_bitrate_Hz */];
+};
+
 union x8h7_can_filter_message
 {
   struct __attribute__((packed))
@@ -76,9 +85,10 @@ int fdcan1_handler(uint8_t const opcode, uint8_t const * data, uint16_t const si
 {
   if (opcode == CAN_INIT)
   {
-    uint32_t const can_bitrate = *((uint32_t *)data);
-    dbg_printf("fdcan1_handler: initializing with frequency %ld\n", can_bitrate);
-    return on_CAN_INIT_Request(&fdcan_1, can_bitrate);
+    union x8h7_can_init_message x8h7_msg;
+    memcpy(x8h7_msg.buf, data, sizeof(x8h7_msg.buf));
+    dbg_printf("fdcan1_handler: initializing with frequency %ld\n", x8h7_msg.field.can_bitrate_Hz);
+    return on_CAN_INIT_Request(&fdcan_1, x8h7_msg.field.can_bitrate_Hz);
   }
   else if (opcode == CAN_DEINIT)
   {
@@ -113,9 +123,10 @@ int fdcan2_handler(uint8_t const opcode, uint8_t const * data, uint16_t const si
 {
   if (opcode == CAN_INIT)
   {
-    uint32_t const can_bitrate = *((uint32_t *)data);
-    dbg_printf("fdcan2_handler: initializing with frequency %ld\n", can_bitrate);
-    return on_CAN_INIT_Request(&fdcan_2, can_bitrate);
+    union x8h7_can_init_message x8h7_msg;
+    memcpy(x8h7_msg.buf, data, sizeof(x8h7_msg.buf));
+    dbg_printf("fdcan2_handler: initializing with frequency %ld\n", x8h7_msg.field.can_bitrate_Hz);
+    return on_CAN_INIT_Request(&fdcan_2, x8h7_msg.field.can_bitrate_Hz);
   }
   else if (opcode == CAN_DEINIT)
   {
