13c2ff8120213b72faf2e826859d4041e48476fc
maidnl <d.aimo@arduino.cc>
2025-10-31 15:45:48 +0100

    Added new functionalities in TX buffer dblBuffer implementation

--- include/double-buffer.h
+++ include/double-buffer.h
@@ -9,7 +9,9 @@ typedef struct dblBuffer {
   uint16_t len_rx;
   uint16_t len_tx;
   uint16_t current_pos_rx;
-  uint16_t current_pos_tx;
+  uint16_t pos_tx_when_writing;
+  uint16_t pos_tx_when_removing;
+  uint16_t tx_to_be_removed;
   void *rx_to_read;
   void *rx_to_receive;
   void *tx_to_write;
@@ -17,20 +19,30 @@ typedef struct dblBuffer {
 
 } DblBuffer_t;
 
-DblBuffer_t *dblBuffer_init(uint8_t *rx1, uint8_t *rx2, uint8_t *tx1, uint8_t *tx2, uint16_t lrx, uint16_t ltx);
+void dblBuffer_init(DblBuffer_t *db, uint8_t *rx1, uint8_t *rx2, uint8_t *tx1, uint8_t *tx2, uint16_t lrx, uint16_t ltx);
 void dblBuffer_destructor(DblBuffer_t *db);
 /* for the application to read from */
 uint8_t *dblBuffer_getRXtoRead(DblBuffer_t *db);
 /* for the peripheral to put data into */
 uint8_t *dblBuffer_getRXtoReceive(DblBuffer_t *db);
 /* for the peripheral to send */
-uint8_t *dblBuffer_getTXtoSend(DblBuffer_t *db);
-/* for the application to write into */
-uint8_t *dblBuffer_getTXtoWrite(DblBuffer_t *db);
+uint8_t *dblBuffer_getTXtoSend(DblBuffer_t *db,uint8_t flag_with_offset);
+/* for the application to write into 
+ * if the flag is != from 0 then the pointer is returned to the current 
+ * available position
+ * if the flag is 0 then the is returned the pointer to beginning of the buffer*/
+uint8_t *dblBuffer_getTXtoWrite(DblBuffer_t *db, uint8_t flag_with_offset);
 void dblBuffer_swapTX(DblBuffer_t *db);
 void dblBuffer_swapRX(DblBuffer_t *db);
 
 void dblBuffer_increaseRXtoReceiveCurrentPos(DblBuffer_t *db, uint16_t offset);
+void dblBuffer_increaseTXtoWritePosWhenWriting(DblBuffer_t *db, uint16_t offset);
+void dblBuffer_increaseTXtoWritePosWhenRemoving(DblBuffer_t *db, uint16_t offset);
+uint16_t dblBuffer_getTXtoWriteWhenWriting(DblBuffer_t *db);
+uint16_t dblBuffer_getTXtoWriteWhenRemoving(DblBuffer_t *db);
+uint16_t dblBuffer_howMuchTXtoWrite(DblBuffer_t *db);
+int dblBuffer_numTxToRemove(DblBuffer_t *db);
 
+#define DBL_BUFF_UART_SIZE 4096
 
 #endif
--- src/double-buffer.c
+++ src/double-buffer.c
@@ -1,33 +1,38 @@
 #include "double-buffer.h"
 #include <stdint.h>
+#include <stdlib.h> // For NULL
+#include <string.h> // For memset
 
 /* --------------------------------------------------------------------------*/
-DblBuffer_t *dblBuffer_init(uint8_t *rx1, uint8_t *rx2, uint8_t *tx1, uint8_t *tx2, uint16_t lrx, uint16_t ltx) {
-  DblBuffer_t *rv = (DblBuffer_t *)malloc(sizeof(DblBuffer_t));
-  if(rv != NULL) {
-    rv->len_rx         = lrx;
-    rv->len_tx          = ltx;
-    rv->current_pos_rx  = 0;
-    rv->current_pos_tx  = 0;
-    rv->rx_to_receive   = rx1;
-    rv->rx_to_read      = rx2;
-    rv->tx_to_send      = tx1;
-    rv->tx_to_write     = tx2;
+// MODIFIED: No longer returns a pointer, just initializes the one passed in.
+void dblBuffer_init(DblBuffer_t *db, uint8_t *rx1, uint8_t *rx2, uint8_t *tx1, uint8_t *tx2, uint16_t lrx, uint16_t ltx) {
+  /* No malloc. The caller must provide a valid db pointer. */
+  if(db == NULL) {
+    return;
+  }
+  
+  db->len_rx               = lrx;
+  db->len_tx               = ltx;
+  db->current_pos_rx       = 0;
+  db->pos_tx_when_writing  = 0;
+  db->rx_to_receive        = rx1;
+  db->rx_to_read           = rx2;
+  db->tx_to_send           = tx1;
+  db->tx_to_write          = tx2;
+  db->pos_tx_when_removing = 0;
+  db->tx_to_be_removed     = 0;
+  
+  if(rx1 != NULL)
     memset(rx1,0x00,lrx);
+  if(rx2 != NULL)
     memset(rx2,0x00,lrx);
+  if(tx1 != NULL)
     memset(tx1,0x00,ltx);
+  if(tx2 != NULL)
     memset(tx2,0x00,ltx);
-  }
-  return rv;
 }
 
-/* --------------------------------------------------------------------------*/
-void dblBuffer_destructor(DblBuffer_t *db) {
-  if(db != NULL) {
-    free(db);
-    db = NULL;
-  }
-}
+
 
 /* --------------------------------------------------------------------------*/
 uint8_t *dblBuffer_getRXtoRead(DblBuffer_t *db) {
@@ -37,11 +42,44 @@ uint8_t *dblBuffer_getRXtoRead(DblBuffer_t *db) {
   return NULL;
 }
 
-
 void dblBuffer_increaseRXtoReceiveCurrentPos(DblBuffer_t *db, uint16_t offset) {
   if(db != NULL) {
     db->current_pos_rx += offset;
   }
+}
+
+void dblBuffer_increaseTXtoWritePosWhenWriting(DblBuffer_t *db, uint16_t offset) {
+  if(db != NULL) {
+    db->pos_tx_when_writing += offset;
+  }
+}
+
+void dblBuffer_increaseTXtoWritePosWhenRemoving(DblBuffer_t *db, uint16_t offset) {
+  if(db != NULL) {
+    db->pos_tx_when_removing += offset;
+  }
+}
+
+uint16_t dblBuffer_getTXtoWriteWhenWriting(DblBuffer_t *db) {
+  if(db != NULL) {
+    return db->pos_tx_when_writing;
+  }
+  return 0;
+}
+
+uint16_t dblBuffer_getTXtoWriteWhenRemoving(DblBuffer_t *db) {
+  if(db != NULL) {
+    return db->pos_tx_when_removing;
+  }
+  return 0;
+}
+
+uint16_t dblBuffer_howMuchTXtoWrite(DblBuffer_t *db) {
+  if(db != NULL) {
+    return db->tx_to_be_removed;
+  }
+  return 0;
+
 }
 
 /* --------------------------------------------------------------------------*/
@@ -53,15 +91,21 @@ uint8_t *dblBuffer_getRXtoReceive(DblBuffer_t *db) {
 }
 
 /* --------------------------------------------------------------------------*/
-uint8_t *dblBuffer_getTXtoSend(DblBuffer_t *db) {
+uint8_t *dblBuffer_getTXtoSend(DblBuffer_t *db, uint8_t flag_with_offset) {
   if(db != NULL) {
+    if(flag_with_offset != 0) {
+      return  db->tx_to_send + db->pos_tx_when_removing;
+    }
     return db->tx_to_send;
   }
   return NULL;
 }
 
-uint8_t *dblBuffer_getTXtoWrite(DblBuffer_t *db) {
+uint8_t *dblBuffer_getTXtoWrite(DblBuffer_t *db, uint8_t flag_with_offset) {
   if(db != NULL) {
+    if(flag_with_offset != 0) {
+      return db->tx_to_write + db->pos_tx_when_writing;
+    } 
     return db->tx_to_write;
   }
   return NULL;
@@ -73,7 +117,18 @@ void dblBuffer_swapTX(DblBuffer_t *db) {
     void *tmp = db->tx_to_send;
     db->tx_to_send = db->tx_to_write;
     db->tx_to_write = tmp;
+    /* save the position */
+    db->tx_to_be_removed = db->pos_tx_when_writing;
+    db->pos_tx_when_removing = 0;
+    db->pos_tx_when_writing = 0; 
   }
+}
+
+int dblBuffer_numTxToRemove(DblBuffer_t *db) {
+  if(db != NULL) {
+    return ((int)db->tx_to_be_removed - (int)db->pos_tx_when_removing);
+  }
+  return 0;
 }
 
 /* --------------------------------------------------------------------------*/
